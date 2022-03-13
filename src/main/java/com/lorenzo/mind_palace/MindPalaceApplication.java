58c9a5a3261306edb2c8c4b08128a3b201e94128
libocheng <1604763055@qq.com>
2022-03-13 16:27:12 +0800

    Version 1.0.0.

---
+++ src/main/java/com/lorenzo/mind_palace/MindPalaceApplication.java
@@ -0,0 +1,13 @@
+package com.lorenzo.mind_palace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindPalaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindPalaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lorenzo/mind_palace/MindPalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenzo.mind_palace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindPalaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
