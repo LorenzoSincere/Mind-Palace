4d660102253d227b39a1f8c66137e5ca5b54833e
libocheng <1604763055@qq.com>
2022-03-16 20:32:09 +0800

    Version 1.3.2 配置axios的BaseUrl读配置文件

--- mindpalace-vue/src/views/Home.vue
+++ mindpalace-vue/src/views/Home.vue
@@ -119,7 +119,7 @@ export default defineComponent({
 
     onMounted(() => {
       console.log("onMounted");
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
+      axios.get("/ebook/list").then((response) => {
       console.log(response);
       const data = response.data;
       ebooks.value = data.content;
