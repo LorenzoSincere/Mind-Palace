5e77151616a1856b924bde08f9c5e06c812c2e1e
libocheng <1604763055@qq.com>
2022-03-20 18:22:47 +0800

    Version 1.8.6 登录功能开发

--- src/main/java/com/lorenzo/mind_palace/request/UserLoginReq.java
+++ src/main/java/com/lorenzo/mind_palace/request/UserLoginReq.java
@@ -1,32 +1,20 @@
 package com.lorenzo.mind_palace.request;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 /**
  * @author libocheng
  */
-public class UserSaveReq {
-    private Long id;
+public class UserLoginReq {
 
-    @NotNull(message = "【用户名】不能为空")
+    @NotEmpty(message = "【用户名】不能为空")
     private String loginName;
 
-    @NotNull(message = "【昵称】不能为空")
-    private String name;
-
     // @Length(min = 6, max = 20, message = "【密码】6~20位")
-    @NotNull(message = "【密码】不能为空")
-    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$", message = "【密码】至少包含 数字和英文，长度6-32")
+    @NotEmpty(message = "【密码】不能为空")
+    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$", message = "【密码】规则不正确")
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getLoginName() {
         return loginName;
@@ -34,14 +22,6 @@ public class UserSaveReq {
 
     public void setLoginName(String loginName) {
         this.loginName = loginName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getPassword() {
@@ -57,9 +37,7 @@ public class UserSaveReq {
         return getClass().getSimpleName() +
                 " [" +
                 "Hash = " + hashCode() +
-                ", id=" + id +
                 ", loginName=" + loginName +
-                ", name=" + name +
                 ", password=" + password +
                 "]";
     }
