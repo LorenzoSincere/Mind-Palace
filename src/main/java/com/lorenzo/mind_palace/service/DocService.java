dd76f07d9041fcff5f4ddfbd6d57a07a6989b012
libocheng <1604763055@qq.com>
2022-03-20 13:56:12 +0800

    Version 1.7.7 后端接口修改，只能查所属当前电子书的文档

--- src/main/java/com/lorenzo/mind_palace/service/DocService.java
+++ src/main/java/com/lorenzo/mind_palace/service/DocService.java
@@ -42,8 +42,9 @@ public class DocService {
     @Resource
     private ContentMapper contentMapper;
 
-    public List<DocQueryResp> all() {
+    public List<DocQueryResp> all(Long ebookId) {
         DocExample docExample = new DocExample();
+        docExample.createCriteria().andEbookIdEqualTo(ebookId);
         docExample.setOrderByClause("sort asc");
         List<Doc> docList = docMapper.selectByExample(docExample);
 
