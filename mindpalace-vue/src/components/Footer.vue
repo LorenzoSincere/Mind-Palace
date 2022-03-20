60a89aa0501ebbd286b29963d536e94d86236047
libocheng <1604763055@qq.com>
2022-03-20 19:32:16 +0800

    Version 1.8.7 使用vuex保存用户信息，其他组件可以获取登录信息

--- mindpalace-vue/src/components/Footer.vue
+++ mindpalace-vue/src/components/Footer.vue
@@ -1,13 +1,24 @@
 <template>
   <a-layout-footer style="text-align: center">
+    <span v-show="user.id">欢迎{{user.name}}创作者，开始分享你的专业知识吧！</span>
     Mind Palace ©2022 Created by Lorenzo
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-  import { defineComponent} from 'vue';
+import store from '@/store';
+import { Tool } from '@/util/tool';
+import { notification } from 'ant-design-vue';
+import {computed, defineComponent, onMounted} from 'vue';
 
   export default defineComponent({
     name: 'Footer',
+    setup() {
+      const user = computed(() => store.state.user);
+
+      return {
+      user
+      }
+    }
   });
 </script>
