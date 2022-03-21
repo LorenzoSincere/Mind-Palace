f96da2311c50e6a4e99b7ca438b57c4849720ed6
libocheng <1604763055@qq.com>
2022-03-21 17:02:04 +0800

    Version 2.0.0 电子书快照表设计&持久层代码生成&前端界面&后端接口

--- doc/demo.sql
+++ doc/demo.sql
@@ -89,3 +89,32 @@ create table `user` (
     primary key (`id`),
     unique key `login_name_unique` (`login_name`)
 ) engine=Innodb default charset=utf8mb4 comment='用户';
+
+# 电子书快照表
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot` (
+    `id` bigint auto_increment not null comment 'id',
+    `ebook_id` bigint not null default 0 comment '电子书id',
+    `date` date not null null comment '快照日期',
+    `view_count` int not null default 0 comment '阅读数',
+    `vote_count` int not null default 0 comment '点赞数',
+    `view_increase` int not null default 0 comment '阅读增长',
+    `vote_increase` int not null default 0 comment '点赞增长',
+    primary key (`id`),
+    unique key `ebook_id_date_unique` (`ebook_id`, `date`)
+) engine=Innodb default charset=utf8mb4 comment='电子书快照表';
+
+insert into ebook_snapshot(ebook_id, `date`, view_count, vote_count, view_increase, vote_increase)
+    select t1.id, curdate(), 0, 0, 0, 0 from ebook t1 where not exists(select 1 from ebook_snapshot t2 where
+    t1.id = t2.ebook_id and t2.`date` = curdate())
+update ebook_snapshot t1, ebook t2
+set t1.view_count = t2.view_count, t1.vote_count = t2.vote_count
+where t1.`date` = curdate();
+select t1.ebook_id, view_count, vote_count from ebook_snapshot t1 where t1.`date` = date_sub(curdate(), interval 1 day);
+
+update ebook_snapshot t1, (select ebook_id, view_count, vote_count from ebook_snapshot
+where `date` = date_sub(curdate(), interval 1 day)) t2
+on t1.ebook_id = t2.ebook_id
+set t1.view_increase = (t1.view_count - ifnull(t2.view_count, 0)),
+    t1.vote_increase = (t1.vote_count - ifnull(t2.vote_count, 0))
+where t1.`date` = curdate(),
---
+++ src/main/java/com/lorenzo/mind_palace/service/EbookSnapshotService.java
@@ -0,0 +1,37 @@
+package com.lorenzo.mind_palace.service;
+
+import com.lorenzo.mind_palace.mapper.EbookSnapshotMapperCount;
+import com.lorenzo.mind_palace.response.StatisticResp;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author libocheng
+ */
+@Service
+public class EbookSnapshotService {
+
+    @Resource
+    private EbookSnapshotMapperCount ebookSnapshotMapperCust;
+
+    public void genSnapshot() {
+        ebookSnapshotMapperCust.genSnapshot();
+    }
+
+    /**
+     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
+     */
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
+    }
+
+    /**
+     * 30天数值统计
+     */
+    public List<StatisticResp> get30Statistic() {
+        return ebookSnapshotMapperCust.get30Statistic();
+    }
+
+}
